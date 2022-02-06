7bff7a1856fc362739512a80f7b04eacf5e39c9f
Carlos Reyes <carlosmxm055@gmail.com>
2022-02-05 20:29:39 -0600

    Version que falta que acepte array en clase clientes

---
+++ src/pruebacsv/DatosClientes.java
@@ -0,0 +1,83 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pruebacsv;
+
+/**
+ *
+ * @author MrX044
+ */
+public class DatosClientes {
+    
+    
+    private String idCliente;
+    private String nombre;
+    private String edad;
+    private String dir;
+    private String tel;
+    private String cel;
+    private String saldo;
+
+    public String getIdCliente() {
+        return idCliente;
+    }
+
+    public void setIdCliente(String idCliente) {
+        this.idCliente = idCliente;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getEdad() {
+        return edad;
+    }
+
+    public void setEdad(String edad) {
+        this.edad = edad;
+    }
+
+    public String getDir() {
+        return dir;
+    }
+
+    public void setDir(String dir) {
+        this.dir = dir;
+    }
+
+    public String getTel() {
+        return tel;
+    }
+
+    public void setTel(String tel) {
+        this.tel = tel;
+    }
+
+    public String getCel() {
+        return cel;
+    }
+
+    public void setCel(String cel) {
+        this.cel = cel;
+    }
+
+    public String getSaldo() {
+        return saldo;
+    }
+
+    public void setSaldo(String saldo) {
+        this.saldo = saldo;
+    }
+
+   
+   
+    
+    
+}
---
+++ src/pruebacsv/Pruebacsv.java
@@ -0,0 +1,98 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pruebacsv;
+
+/**
+ *
+ * @author MrX044
+ */
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Scanner;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.swing.JFileChooser;
+public class Pruebacsv {
+
+    
+    // muestra el cuadro de diálogo de archivos, para que el usuario pueda elegir el archivo a abrir
+JFileChooser selectorArchivos = new JFileChooser();
+
+
+
+// indica cual fue la accion de usuario sobre el jfilechooser
+
+
+    /**
+     * @param args the command line arguments
+     */
+    
+    public static void main(String[] args) throws FileNotFoundException {
+          //manda a llamar a clase clientes
+         DatosClientes clientes = new DatosClientes();
+
+         
+         int opc=0;  
+         
+         //CON ESTO OBTENEMOS LA RUTA DEL ARCHIVO
+        JFileChooser fileChooser = new JFileChooser();
+        fileChooser.showOpenDialog(fileChooser);
+        String ruta = fileChooser.getSelectedFile().getAbsolutePath(); 
+        
+        //SE USA PARA PODER LEER Y TENER EN MEMORIA LOS DATOS
+        String line = "";
+        BufferedReader br = new BufferedReader(new FileReader(ruta));
+        
+        //FUNCION QUE LEE Y RECORRE EL CSV
+    try {
+        while((line = br.readLine()) != null){
+            String values[]= line.split(";"); //COMO NO ME DABA CON COMA POR USO DE EXCEL, LO USÉ CON ;
+           
+           //ACÁ GUARDO LOS DATOS EN LA CLASE DE CLIENTES
+           
+            clientes.setIdCliente(values[0].toString()); //POSICION 0 ES DE ID
+            clientes.setNombre(values[0].toString()); //POSICION 1 ES DE NOMBRE
+            clientes.setEdad(values[0].toString()); //POSICION 2 ES DE EDAD
+            clientes.setDir(values[0].toString()); //POSICION 3 ES DE DIRECCION
+            clientes.setTel(values[0].toString()); //POSICION 4 ES DE TELEFONO
+            clientes.setCel(values[0].toString()); //POSICION 5 ES DE CELULAR
+            clientes.setSaldo(values[0].toString()); //POSICION 6 ES SALDO
+            
+            System.out.println("id cliente perra: " +clientes.getIdCliente() +"\n\n\n");
+            
+           System.out.println("[iD: \t"+ values[0]+"\tNombre: "+ values[1]+"\tEdad:"+ values[2]+"\tDireccion: "+ values[3]+"\t Numcel: "+ values[4]+"\t Telefono: "+ values[5]+"\tSaldo: "+ values[6]+"]");
+           
+           switch(opc){
+                
+               case 1: 
+                   System.out.println("\"Dame el id del cliente: \"");
+                   Scanner lecturaID = new Scanner (System.in);
+                     if(lecturaID.equals(values[0])){
+                         System.out.println("El maestro me la pela1");};
+                         break;
+           
+           }
+           
+        
+        }        
+    } catch (IOException ex) {
+        Logger.getLogger(Pruebacsv.class.getName()).log(Level.SEVERE, null, ex);
+    }
+        
+        
+       
+                                             
+            
+        //FALTA POR HACER
+        //CONVERTIR EN ARRAY LOS DATOS DE LA CLASE CLIENTES Y QUE LOS ACEPTE
+        
+    }//fin metodo main
+    
+    
+}//fin clase main
